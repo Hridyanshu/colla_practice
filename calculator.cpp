70cdb696329f120f531d2cffd1d65f9813654a03
Hridyanshu <hridyanshusharma@gmail.com>
2019-04-13 12:01:52 +0530

    Committed addition function

---
+++ calculator.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class Calculator
+{
+	public:
+	double addition(double,double);
+	double subtraction(double,double);
+	double multiply(double,double);
+	double divide(double,double);	
+};
+double addition(double,double)
+{
+	double a,b,add;
+	cout<<"Enter 2 Numbers"<<endl;
+	cin>>a>>b;
+	add=a+b;
+	cout<<"Addition is: "<<add<<endl;
+}
